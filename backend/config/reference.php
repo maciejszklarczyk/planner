6ff427dfe499c8624babb15bb46fdf3f512a3c1b
Maciej Szklarczyk <maciej.szklarczyk@hotmail.com>
2026-01-31 22:45:56 +0100

    PLA-9 Update session cookie defaults and production settings
    - Change session cookie defaults: `secure` to `true` and `samesite` to `none`.
    - Configure production session cookies with `cookie_secure: true`, `cookie_samesite: none`, and `cookie_domain: .msolve.it`.

--- backend/config/reference.php
+++ backend/config/reference.php
@@ -1192,9 +1192,9 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *             lifetime?: int|Param, // Default: 31536000
  *             path?: scalar|null|Param, // Default: "/"
  *             domain?: scalar|null|Param, // Default: null
- *             secure?: true|false|"auto"|Param, // Default: null
+ *             secure?: true|false|"auto"|Param, // Default: true
  *             httponly?: bool|Param, // Default: true
- *             samesite?: null|"lax"|"strict"|"none"|Param, // Default: "strict"
+ *             samesite?: null|"lax"|"strict"|"none"|Param, // Default: "none"
  *             always_remember_me?: bool|Param, // Default: false
  *             remember_me_parameter?: scalar|null|Param, // Default: "_remember_me"
  *         },
